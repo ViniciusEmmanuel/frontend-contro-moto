fe1b60ca8b46850b552f0fb1402b83aef8d4ff55
Vinicius Emmanuel <vinicius.teixeira.m@gmail.com>
2020-05-02 06:11:03 -0300

    add name e user header

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,15 +1,20 @@
 import React, { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FiPower } from 'react-icons/fi';
 
 import { requestToLogout } from '../../store/redux/logon/actions';
+import { Istate, IinicialState } from '../../interfaces/redux/logon';
 
 import { Header as HeaderMain } from './styles';
 
 export const Header = () => {
   const history = useHistory();
   const dispatch = useDispatch();
+
+  const { name, user } = useSelector<Istate, IinicialState>(
+    (state) => state.logon
+  );
 
   const logout = useCallback((): void => {
     dispatch(requestToLogout());
@@ -20,7 +25,10 @@ export const Header = () => {
     <HeaderMain>
       <div className="content">
         <span>
-          Rwr <strong>Usuario Novo</strong>
+          RWR::
+          <strong>
+            {name} ({user})
+          </strong>
         </span>
 
         <button type="button" className="btn-icon" onClick={logout}>
